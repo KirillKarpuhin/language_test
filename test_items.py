3ef2a29dc192444fbf063028051e6e0acb8be43c
KirillKarpuhin <k-rreeaall@mail.ru>
2025-03-12 21:17:36 +0300

    Выбор языка сайта

--- test_items.py
+++ test_items.py
@@ -1,11 +1,17 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import time
 
 def test_add_to_basket_button(browser):
     # Открываем страницу товара
     browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
+    time.sleep(15)
 
     # Проверяем, что кнопка добавления в корзину присутствует на странице
-    add_to_basket = browser.find_element(By.CSS_SELECTOR, "button.btn-add-to-basket")
-    add_to_basket.click()
+    add_to_basket_button = browser.find_elements(By.CSS_SELECTOR, "button.btn-add-to-basket")
+    assert len(add_to_basket_button) > 0, "Кнопка добавления в корзину не найдена"
+
+    print("Кнопка добавления в корзину найдена!")
+
+    # запуск происходит pytest --language=es --browser_name=chrome -s -v test_items.py
